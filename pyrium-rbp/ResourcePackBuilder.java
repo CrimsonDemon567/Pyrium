6a17b871f7fc6c0baab7c3d2a8e0e0fe3e6bdc58
CrimsonDemon567 <vincentnoll52@icloud.com>
2025-12-18 18:00:47 +0100

    Create ResourcePackBuilder with buildIfNeeded method
    Added a stub method for building resource packs.

---
+++ pyrium-rbp/ResourcePackBuilder.java
@@ -0,0 +1,7 @@
+package io.pyrium.rpb;
+
+public final class ResourcePackBuilder {
+  public static void buildIfNeeded() {
+    // Stub: later compile assets from pymods, versioned by mc_version policy
+  }
+}
