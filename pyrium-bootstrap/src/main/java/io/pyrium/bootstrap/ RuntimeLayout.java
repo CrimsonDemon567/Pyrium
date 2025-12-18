c0ff18f70ad6f1a485768e1721978c259a3083e4
CrimsonDemon567 <vincentnoll52@icloud.com>
2025-12-18 20:39:21 +0100

    Add modsDir to RuntimeLayout record

--- pyrium-bootstrap/src/main/java/io/pyrium/bootstrap/ RuntimeLayout.java
+++ pyrium-bootstrap/src/main/java/io/pyrium/bootstrap/ RuntimeLayout.java
@@ -2,10 +2,12 @@ package io.pyrium.bootstrap;
 
 import java.nio.file.*;
 
-public record RuntimeLayout(Path versionDir, Path serverJar) {
+public record RuntimeLayout(Path versionDir, Path serverJar, Path modsDir) {
   public static RuntimeLayout prepare(Resolver.Artifact art) throws Exception {
     Path dir = Paths.get(".pyrium/runtime/" + art.versionTag());
     Files.createDirectories(dir);
-    return new RuntimeLayout(dir, art.path());
+    Path mods = dir.resolve("mods");
+    Files.createDirectories(mods);
+    return new RuntimeLayout(dir, art.path(), mods);
   }
 }
