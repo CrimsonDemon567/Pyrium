58980137574e70856eb0f87b2070a446c9a99b50
CrimsonDemon567 <vincentnoll52@icloud.com>
2025-12-18 21:36:25 +0100

    Update PyBCModule.java

--- pyrium-core/src/main/java/io/pyrium/core/PyBCModule.java
+++ pyrium-core/src/main/java/io/pyrium/core/PyBCModule.java
@@ -1,14 +1,18 @@
 package io.pyrium.core;
 
 import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.file.Path;
 import java.util.*;
 
 public final class PyBCModule {
   public static final int MAGIC = 0x50594243; // 'PYBC'
-  public static final int VERSION = 1;
+  public static final int VERSION = 2;        // upgraded format
+
+  // Constant pool for strings to reduce size
+  public static final class ConstPool {
+    public final List<String> strings;
+    ConstPool(List<String> strings){ this.strings = strings; }
+    String get(int idx){ return strings.get(idx); }
+  }
 
   public static final class Function {
     public final String name;
@@ -17,7 +21,9 @@ public final class PyBCModule {
   }
 
   public enum OpCode {
-    NOP(0), LOG(1), MUL_ENTITY_SPEED(2);
+    NOP(0), LOG(1), MUL_ENTITY_SPEED(2),
+    BROADCAST(3), EXEC_CMD(4), GIVE_ITEM(5),
+    NBT_SET(6), NBT_GET(7), FILTER_ENTITIES_REGION(8);
     public final int id; OpCode(int id){this.id=id;}
     static OpCode of(int id){
       for (var o : values()) if (o.id == id) return o;
@@ -26,15 +32,22 @@ public final class PyBCModule {
   }
 
   public static final class Op {
-    public final OpCode code; public final String a; public final double d;
-    public Op(OpCode code, String a, double d){ this.code = code; this.a = a; this.d = d; }
+    public final OpCode code;
+    public final int sIdxA;      // string index A
+    public final int sIdxB;      // string index B
+    public final double d;       // numeric payload
+    public final long i;         // integer payload
+    public Op(OpCode code, int sIdxA, int sIdxB, double d, long i){
+      this.code = code; this.sIdxA = sIdxA; this.sIdxB = sIdxB; this.d = d; this.i = i;
+    }
   }
 
   public final String moduleName;
+  public final ConstPool pool;
   public final Map<String, Function> functions;
 
-  public PyBCModule(String moduleName, Map<String, Function> functions) {
-    this.moduleName = moduleName; this.functions = functions;
+  public PyBCModule(String moduleName, ConstPool pool, Map<String, Function> functions) {
+    this.moduleName = moduleName; this.pool = pool; this.functions = functions;
   }
 
   public static PyBCModule read(Path file) throws IOException {
@@ -43,7 +56,14 @@ public final class PyBCModule {
       if (magic != MAGIC) throw new IOException("Bad magic in " + file);
       int ver = in.readInt();
       if (ver != VERSION) throw new IOException("Unsupported pybc version: " + ver);
+
       String modName = in.readUTF();
+
+      int poolCount = in.readInt();
+      List<String> strings = new ArrayList<>(poolCount);
+      for (int k=0;k<poolCount;k++){ strings.add(in.readUTF()); }
+      ConstPool pool = new ConstPool(strings);
+
       int fnCount = in.readInt();
       Map<String, Function> fns = new HashMap<>();
       for (int i=0;i<fnCount;i++){
@@ -52,13 +72,15 @@ public final class PyBCModule {
         List<Op> ops = new ArrayList<>(opCount);
         for (int j=0;j<opCount;j++){
           int opcode = in.readInt();
-          String a = in.readUTF();
+          int sA = in.readInt();
+          int sB = in.readInt();
           double d = in.readDouble();
-          ops.add(new Op(OpCode.of(opcode), a, d));
+          long i64 = in.readLong();
+          ops.add(new Op(OpCode.of(opcode), sA, sB, d, i64));
         }
         fns.put(fname, new Function(fname, ops));
       }
-      return new PyBCModule(modName, fns);
+      return new PyBCModule(modName, pool, fns);
     }
   }
 }
