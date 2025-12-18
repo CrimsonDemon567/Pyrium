1d83b9631c8ee95ba6dda8b3bd8d18729af729d6
CrimsonDemon567 <vincentnoll52@icloud.com>
2025-12-18 21:42:18 +0100

    Update  emit_pybc.py

--- pyrium-aot/pyrium-aot/ emit_pybc.py
+++ pyrium-aot/pyrium-aot/ emit_pybc.py
@@ -1,24 +1,34 @@
 import struct
 
 MAGIC = 0x50594243  # 'PYBC'
-VERSION = 1
+VERSION = 2
 
 def emit_ir_to_pybc(ir, out_path:str):
+    # Build string pool
+    pool = []
+    for fn in ir.functions:
+        for op in fn.ops:
+            for s in (op.a, op.b):
+                if s and s not in pool: pool.append(s)
+
     with open(out_path, "wb") as out:
         out.write(struct.pack(">II", MAGIC, VERSION))
-        name = ir.name.encode("utf-8")
-        out.write(struct.pack(">H", len(name)))
-        out.write(name)
+        out.write(_utf(ir.name))
+        out.write(struct.pack(">I", len(pool)))
+        for s in pool: out.write(_utf(s))
         out.write(struct.pack(">I", len(ir.functions)))
         for fn in ir.functions:
-            fname = fn.name.encode("utf-8")
-            out.write(struct.pack(">H", len(fname)))
-            out.write(fname)
+            out.write(_utf(fn.name))
             out.write(struct.pack(">I", len(fn.ops)))
             for op in fn.ops:
                 out.write(struct.pack(">I", int(op.code)))
-                astr = (op.a or "").encode("utf-8")
-                out.write(struct.pack(">H", len(astr)))
-                out.write(astr)
+                sA = pool.index(op.a) if op.a in pool else -1
+                sB = pool.index(op.b) if op.b in pool else -1
+                out.write(struct.pack(">ii", sA, sB))
                 out.write(struct.pack(">d", float(op.d)))
+                out.write(struct.pack(">q", int(op.i)))
     return out_path
+
+def _utf(s:str):
+    b = s.encode("utf-8")
+    return struct.pack(">H", len(b)) + b
