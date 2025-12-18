3a41a2cd89e010721cfcd9c44fd0e8783e0adb3f
CrimsonDemon567 <vincentnoll52@icloud.com>
2025-12-18 21:12:37 +0100

    Update cli.py

--- pyrium-aot/pyrium-aot/cli.py
+++ pyrium-aot/pyrium-aot/cli.py
@@ -1,12 +1,15 @@
-import argparse
+import argparse, os
 from .compiler import compile_python_to_ir
-from .emit_class import emit_ir_to_class
+from .emit_pybc import emit_ir_to_pybc
 
 def main():
     p = argparse.ArgumentParser()
     p.add_argument("--in", dest="inp", required=True, help="Path to Python mod")
-    p.add_argument("--out", dest="out", required=True, help="Output directory for class files")
+    p.add_argument("--out", dest="out", required=True, help="Directory to place .pybc")
     args = p.parse_args()
+
     ir = compile_python_to_ir(args.inp)
-    cls = emit_ir_to_class(ir, args.out)
-    print(f"[Pyrium AOT] Emitted: {cls}")
+    os.makedirs(args.out, exist_ok=True)
+    out_file = os.path.join(args.out, "mod.pybc")
+    emit_ir_to_pybc(ir, out_file)
+    print(f"[Pyrium AOT] Emitted: {out_file}")
