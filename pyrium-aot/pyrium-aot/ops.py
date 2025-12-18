6f1539b6b13cf3c7cbea178867814478e6d2d632
CrimsonDemon567 <vincentnoll52@icloud.com>
2025-12-18 21:40:37 +0100

    Update ops.py

--- pyrium-aot/pyrium-aot/ops.py
+++ pyrium-aot/pyrium-aot/ops.py
@@ -4,3 +4,9 @@ class OpCode(IntEnum):
     NOP = 0
     LOG = 1
     MUL_ENTITY_SPEED = 2
+    BROADCAST = 3
+    EXEC_CMD = 4
+    GIVE_ITEM = 5
+    NBT_SET = 6
+    NBT_GET = 7
+    FILTER_ENTITIES_REGION = 8
